381e99fba257b2baff96ce341f1dd22f92917cb5
znsndev <dkolesnikovd@gmail.com>
2023-12-28 20:40:14 +0700

    Bottle class added

---
+++ Product/Bottle.py
@@ -0,0 +1,12 @@
+from Product import Product
+
+
+class Bottle(Product):
+	__volume: float
+
+	def __init__(self, name: str, price:float, volume: float)
+		super().__init__(name, price)
+		self.__volume = volume
+
+	def __str__(self):
+		return super().__str__() + f"Volume: {self.__volume}\n"
